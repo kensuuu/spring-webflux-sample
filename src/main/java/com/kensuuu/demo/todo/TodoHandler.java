253a549367294b3bfc88402a5e26d7ffe7354b8a
kensuke.suzuki <kensuke.11288@gmail.com>
2024-02-07 23:55:03 +0900

    update

--- src/main/java/com/kensuuu/demo/todo/TodoHandler.java
+++ src/main/java/com/kensuuu/demo/todo/TodoHandler.java
@@ -1,6 +1,6 @@
 package com.kensuuu.demo.todo;
 
-import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -8,16 +8,22 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import static com.kensuuu.demo.Handlers.handleQueryParam;
+
 @Component
+@Slf4j
 public class TodoHandler {
 
     public RouterFunction<ServerResponse> routes() {
         return RouterFunctions.route()
-                .GET("/todo/{id}", this::get)
+                .GET("/todo", this::get)
                 .build();
     }
 
     public Mono<ServerResponse> get(ServerRequest serverRequest) {
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).build();
+        return handleQueryParam(serverRequest, GetTodoRequest.class, request -> {
+            log.info(request.toString());
+            return Mono.empty();
+        });
     }
 }
