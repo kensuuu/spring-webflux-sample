a750fc3e020df04c524f7c6745eee56306a7e1ed
kensuke.suzuki <kensuke.11288@gmail.com>
2024-02-10 20:54:16 +0900

    update

--- src/main/java/com/kensuuu/demo/todo/Status.java
+++
@@ -1,8 +0,0 @@
-package com.kensuuu.demo.todo;
-
-public enum Status {
-    TODO,
-    DOING,
-    DONE
-    ;
-}
--- src/main/java/com/kensuuu/demo/todo/TodoHandler.java
+++ src/main/java/com/kensuuu/demo/todo/TodoHandler.java
@@ -19,6 +19,9 @@ public class TodoHandler {
     public RouterFunction<ServerResponse> routes() {
         return RouterFunctions.route()
                 .GET("/todo/{id}", this::get)
+                .POST("/todo", this::create)
+                .PUT("/todo/{id}", this::update)
+                .DELETE("/todo/{id}", this::delete)
                 .build();
     }
 
@@ -26,6 +29,29 @@ public class TodoHandler {
         var id = Long.parseLong(serverRequest.pathVariable("id"));
         return todoRepository.findById(id)
                 .flatMap(todo -> ServerResponse.ok().bodyValue(todo))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> create(ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(Todo.class)
+                .flatMap(todoRepository::save)
+                .flatMap(response -> ServerResponse.ok().bodyValue(response));
+    }
+
+    public Mono<ServerResponse> update(ServerRequest serverRequest) {
+        var id = Long.parseLong(serverRequest.pathVariable("id"));
+        return serverRequest.bodyToMono(Todo.class)
+                .flatMap(todo -> todoRepository.findById(id)
+                        .flatMap(existingTodo -> todoRepository.save(todo)))
+                .flatMap(response -> ServerResponse.ok().bodyValue(response))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> delete(ServerRequest serverRequest) {
+        var id = Long.parseLong(serverRequest.pathVariable("id"));
+        return todoRepository.findById(id)
+                .flatMap(todo -> todoRepository.delete(todo)
+                        .then(ServerResponse.ok().build()))
                 .switchIfEmpty(ServerResponse.notFound().build());
     }
 }
